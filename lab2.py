cad0ad93d15369bc4c15b5c28094bbc622b87c80
Valiantsin Samsonau <62325681+SamaritaninS@users.noreply.github.com>
2021-12-10 18:34:30 +0300

    Update lab

--- lab2.py
+++ lab2.py
@@ -3,8 +3,6 @@ import numpy as np
 from math import factorial
 from matplotlib import pyplot as plt
 
-fig, axs = plt.subplots(2)
-
 
 class SMO(object):
     def __init__(self, env, number_channels):
@@ -119,23 +117,19 @@ def theoretic_probabilities(number_channels, queue_length, app_flow, serv_flow, 
     print("Theoretical probability of queuing: ", p_queque)
     final_propability.append(p_queque)
     relative_t = 1 - p
-    final_propability.append(relative_t)
     print("Theoretical relative throughput: ", relative_t)
     absolute_t = relative_t * app_flow
     final_propability.append(absolute_t)
     print("Theoretical absolute throughput: ", absolute_t)
     n_people_queque = sum([i * pn * (ro ** i) / np.prod([number_channels + l * betta for l in range(1, i + 1)]) for
                            i in range(1, queue_length + 1)])
-    final_propability.append(n_people_queque)
     print("Average applications in queue: ", n_people_queque)
     K_av = sum([index * p0 * (ro ** index) / factorial(index) for index in range(1, number_channels + 1)]) + sum(
         [(number_channels + index) * pn * ro ** index / np.prod(
             np.array([number_channels + l * betta for l in range(1, index + 1)])) for
          index in range(1, queue_length + 1)])
-    final_propability.append(K_av)
     print("Average applications in SMO: ", K_av)
     n_average = relative_t * ro
-    final_propability.append(n_average)
     print("Average number of full channels: ", n_average)
     T_queque = n_people_queque / app_flow
     final_propability.append(T_queque)
@@ -167,39 +161,213 @@ def empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_l
     print("Empiric probability of queuing: ", p_queued)
     final_propability.append(p_queued)
     relative_t = 1 - p_fail
-    final_propability.append(relative_t)
     print("Empiric relative throughput: ", relative_t)
     absolute_t = relative_t * app_flow
     final_propability.append(absolute_t)
     print("Empiric absolute throughput: ", absolute_t)
     n_people_queque = average_number_applications(queue_list)
-    final_propability.append(n_people_queque)
     number_av = average_smo_number_applications(smo_list)
-    final_propability.append(number_av)
     n_average = relative_t * app_flow / serv_flow
-    final_propability.append(n_average)
     print("Average number of full channels: ", n_average)
-    time_queque = average_queue_application_time(queue_time)
-    final_propability.append(time_queque)
+    time_queue = average_queue_application_time(queue_time)
+    final_propability.append(time_queue)
     time_smo = average_smo_application_time(wait_times)
     final_propability.append(time_smo)
-    axs[0].hist(wait_times, 50)
-    axs[0].set_title('Wait times')
-    axs[1].hist(queue_list, 50)
     return final_propability
 
 
-if __name__ == '__main__':
-    # --------Variables----------#
-    number_channels = 2
-    app_flow = 2
-    serv_flow = 1
-    queue_length = 2
-    queue_flow = 1
-    # -----------------------------#
-
-    wait_times, queue_list, queue_time, cancel_list, smo_list = create_SMO(number_channels, app_flow, serv_flow, queue_flow, queue_length, 10000)
-    empiric_propability = empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_list, number_channels, queue_length, app_flow, serv_flow)
-    print("-------------------------------------------------------")
-    theoretic_propability = theoretic_probabilities(number_channels, queue_length, app_flow, serv_flow, queue_flow)
+def show_difference(empirics, theoretics):
+    fig, ax = plt.subplots()
+    ax.bar([i - 0.2 for i in range(len(empirics))], empirics, tick_label=["p0", "p1", "p2", "p3", "p4", "p_reject", "p_queue", "absolute_throughput", "time_queue", "time_smo"], width=0.4, color='blue',
+           label='Empiric')
+    ax.bar([i + 0.2 for i in range(len(empirics))], theoretics, tick_label=["p0", "p1", "p2", "p3", "p4", "p_reject", "p_queue", "absolute_throughput", "time_queue", "time_smo"], width=0.4, color='red', label='Theoretical')
+    ax.set_title('Empiric and theoretic difference')
+    fig.set_figwidth(15)
+    fig.set_figheight(6)
+    plt.legend()
     plt.show()
+
+
+def plot_queue_system_probabilities(counts, theoretical_probabilities, interval_count):
+  intervals = np.array_split(counts, interval_count)
+  for i in range(1, len(intervals)):
+    intervals[i] = np.append(intervals[i], intervals[i - 1])
+  for i in range(len(theoretical_probabilities)):
+    if i < n+m+1:
+        interval_probabilities = []
+        for interval in intervals:
+          interval_probabilities.append(len(interval[interval == i]) / len(interval))
+        plt.figure(figsize=(5, 5))
+        plt.bar(range(len(interval_probabilities)), interval_probabilities)
+        plt.title(f"Probabilitiy {i}")
+        plt.axhline(y = theoretical_probabilities[i], xmin=0, xmax=len(interval_probabilities), color='purple')
+        plt.show()
+
+
+def show_changes(empirics1, empirics2, empirics3, value_name, values):
+    fig, ax = plt.subplots()
+    labels = ["P[0]", 'P[1]', 'P[2]', 'P[3]', 'P[4]']
+    label1 = value_name + " = " + str(values[0])
+    ax.bar([i for i in range(len(empirics1))], empirics1, tick_label=labels, width=0.3, color='blue',
+           label=label1)
+    label2 = value_name + " = " + str(values[1])
+    ax.bar([i + 0.3 for i in range(len(empirics1))], empirics2, tick_label=labels, width=0.3, color='yellow',
+           label=label2)
+    label3 = value_name + " = " + str(values[2])
+    ax.bar([i + 0.6 for i in range(len(empirics1))], empirics3, tick_label=labels, width=0.3, color='red',
+           label=label3)
+    ax.set_title('Empiric values changes')
+    fig.set_figwidth(19)
+    fig.set_figheight(6)
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    n = 2
+    u = 1
+    ro = 1
+    m = 2
+
+values = [1, 4, 7]
+lambd = values[0]
+wait_times, queue_list, queue_time, cancel_list, smo_list = create_SMO(n, lambd, u, ro, m, 10000)
+
+empiric_probability = empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_list, n, m, lambd, u)
+theoretic_probability = theoretic_probabilities(n, m, lambd, u, ro)
+show_difference(empiric_probability, theoretic_probability)
+plot_queue_system_probabilities(smo_list, theoretic_probability, 1000)
+empirics1 = []
+empirics1.append(empiric_probability[0])
+empirics1.append(empiric_probability[1])
+empirics1.append(empiric_probability[2])
+empirics1.append(empiric_probability[3])
+empirics1.append(empiric_probability[4])
+
+lambd = values[1]
+empiric_probability.clear()
+wait_times, queue_list, queue_time, cancel_list, smo_list = create_SMO(n, lambd, u, ro, m, 10000)
+empiric_probability = empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_list, n, m, lambd, u)
+theoretic_probability = theoretic_probabilities(n, m, lambd, u, ro)
+empirics2 = []
+empirics2.append(empiric_probability[0])
+empirics2.append(empiric_probability[1])
+empirics2.append(empiric_probability[2])
+empirics2.append(empiric_probability[3])
+empirics2.append(empiric_probability[4])
+
+lambd = values[2]
+empiric_probability.clear()
+wait_times, queue_list, queue_time, cancel_list, smo_list = create_SMO(n, lambd, u, ro, m, 10000)
+empiric_probability = empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_list, n, m, lambd, u)
+theoretic_probability = theoretic_probabilities(n, m, lambd, u, ro)
+empirics3 = []
+empirics3.append(empiric_probability[0])
+empirics3.append(empiric_probability[1])
+empirics3.append(empiric_probability[2])
+empirics3.append(empiric_probability[3])
+empirics3.append(empiric_probability[4])
+
+
+show_changes(empirics1, empirics2, empirics3, "Lambda", values)
+
+#---------------------------------------------------------------------------------------------------------
+lambd = 2
+empirics1.clear()
+empirics3.clear()
+empirics2.clear()
+empiric_probability.clear()
+values.clear()
+values = [1, 4, 7]
+
+u = values[0]
+wait_times, queue_list, queue_time, cancel_list, smo_list = create_SMO(n, lambd, u, ro, m, 10000)
+empiric_probability = empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_list, n, m, lambd, u)
+theoretic_probability = theoretic_probabilities(n, m, lambd, u, ro)
+empirics1 = []
+empirics1.append(empiric_probability[0])
+empirics1.append(empiric_probability[1])
+empirics1.append(empiric_probability[2])
+empirics1.append(empiric_probability[3])
+empirics1.append(empiric_probability[4])
+
+
+u = values[1]
+empiric_probability.clear()
+wait_times, queue_list, queue_time, cancel_list, smo_list = create_SMO(n, lambd, u, ro, m, 10000)
+empiric_probability = empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_list, n, m, lambd, u)
+theoretic_probability = theoretic_probabilities(n, m, lambd, u, ro)
+empirics2 = []
+empirics2.append(empiric_probability[0])
+empirics2.append(empiric_probability[1])
+empirics2.append(empiric_probability[2])
+empirics2.append(empiric_probability[3])
+empirics2.append(empiric_probability[4])
+
+
+u = values[2]
+empiric_probability.clear()
+wait_times, queue_list, queue_time, cancel_list, smo_list = create_SMO(n, lambd, u, ro, m, 10000)
+empiric_probability = empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_list, n, m, lambd, u)
+theoretic_probability = theoretic_probabilities(n, m, lambd, u, ro)
+empirics3 = []
+empirics3.append(empiric_probability[0])
+empirics3.append(empiric_probability[1])
+empirics3.append(empiric_probability[2])
+empirics3.append(empiric_probability[3])
+empirics3.append(empiric_probability[4])
+
+
+show_changes(empirics1, empirics2, empirics3, "u", values)
+
+
+#---------------------------------------------------------------------------------------------------------
+u = 2
+empirics1.clear()
+empirics3.clear()
+empirics2.clear()
+empiric_probability.clear()
+values.clear()
+values = [2, 3, 4]
+
+ro = values[0]
+wait_times, queue_list, queue_time, cancel_list, smo_list = create_SMO(n, lambd, u, ro, m, 10000)
+empiric_probability = empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_list, n, m, lambd, u)
+theoretic_probability = theoretic_probabilities(n, m, lambd, u, ro)
+empirics1 = []
+empirics1.append(empiric_probability[0])
+empirics1.append(empiric_probability[1])
+empirics1.append(empiric_probability[2])
+empirics1.append(empiric_probability[3])
+empirics1.append(empiric_probability[4])
+
+
+ro = values[1]
+empiric_probability.clear()
+wait_times, queue_list, queue_time, cancel_list, smo_list = create_SMO(n, lambd, u, ro, m, 10000)
+empiric_probability = empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_list, n, m, lambd, u)
+theoretic_probability = theoretic_probabilities(n, m, lambd, u, ro)
+empirics2 = []
+empirics2.append(empiric_probability[0])
+empirics2.append(empiric_probability[1])
+empirics2.append(empiric_probability[2])
+empirics2.append(empiric_probability[3])
+empirics2.append(empiric_probability[4])
+
+
+ro = values[2]
+empiric_probability.clear()
+wait_times, queue_list, queue_time, cancel_list, smo_list = create_SMO(n, lambd, u, ro, m, 10000)
+empiric_probability = empiric_probabilities(cancel_list, queue_list, queue_time, wait_times, smo_list, n, m, lambd, u)
+theoretic_probability = theoretic_probabilities(n, m, lambd, u, ro)
+empirics3 = []
+empirics3.append(empiric_probability[0])
+empirics3.append(empiric_probability[1])
+empirics3.append(empiric_probability[2])
+empirics3.append(empiric_probability[3])
+empirics3.append(empiric_probability[4])
+
+
+show_changes(empirics1, empirics2, empirics3, "ro", values)
+
+
